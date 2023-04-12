0709cac6d9082799661540b7a77ce3ba4e659c68
painnick <painnick@gmail.com>
2023-04-13 00:03:24 +0900

    UPDATE. 매크로 상수 처리

--- RC1_HC06.ino
+++ RC1_HC06.ino
@@ -11,6 +11,11 @@
 #define BT_RX_PIN 8
 #define BT_TX_PIN 7
 
+// 0~255
+#define POWER_MAX 255
+#define POWER_MID 127
+#define POWER_OFF 0
+
 SoftwareSerial bt(BT_RX_PIN, BT_TX_PIN);
 
 void setup() {
@@ -27,10 +32,10 @@ void setup() {
   pinMode(RIGHT_WHEEL_PIN2, OUTPUT);
 
   // 정지
-  analogWrite(LEFT_WHEEL_PIN1, 0);
-  analogWrite(RIGHT_WHEEL_PIN1, 0);
-  analogWrite(LEFT_WHEEL_PIN2, 0);
-  analogWrite(RIGHT_WHEEL_PIN2, 0);
+  analogWrite(LEFT_WHEEL_PIN1, POWER_OFF);
+  analogWrite(RIGHT_WHEEL_PIN1, POWER_OFF);
+  analogWrite(LEFT_WHEEL_PIN2, POWER_OFF);
+  analogWrite(RIGHT_WHEEL_PIN2, POWER_OFF);
 }
 
 unsigned long checked = 0;
@@ -40,10 +45,10 @@ void loop() {
   if (now - checked > 2000) {
     Serial.println(".");
 
-    analogWrite(LEFT_WHEEL_PIN1, 0); // Off
-    analogWrite(RIGHT_WHEEL_PIN1, 0);
-    analogWrite(LEFT_WHEEL_PIN2, 0);
-    analogWrite(RIGHT_WHEEL_PIN2, 0);
+    analogWrite(LEFT_WHEEL_PIN1, POWER_OFF); // Off
+    analogWrite(RIGHT_WHEEL_PIN1, POWER_OFF);
+    analogWrite(LEFT_WHEEL_PIN2, POWER_OFF);
+    analogWrite(RIGHT_WHEEL_PIN2, POWER_OFF);
 
     checked = now;
   }
@@ -56,20 +61,20 @@ void loop() {
       case 'U': // 전진
         checked = now;
         Serial.println("UP");
-        analogWrite(LEFT_WHEEL_PIN1, 255); // On(MAX!)
-        analogWrite(RIGHT_WHEEL_PIN1, 255);
+        analogWrite(LEFT_WHEEL_PIN1, POWER_MAX); // On(MAX!)
+        analogWrite(RIGHT_WHEEL_PIN1, POWER_MAX);
         break;
       case 'L': // 좌회전
         checked = now;
         Serial.println("Left");
-        analogWrite(LEFT_WHEEL_PIN1, 255);
-        analogWrite(RIGHT_WHEEL_PIN1, 63);
+        analogWrite(LEFT_WHEEL_PIN1, POWER_MAX);
+        analogWrite(RIGHT_WHEEL_PIN1, POWER_MID);
         break;
       case 'R': // 우회전
         checked = now;
         Serial.println("Right");
-        analogWrite(LEFT_WHEEL_PIN1, 63);
-        analogWrite(RIGHT_WHEEL_PIN1, 255);
+        analogWrite(LEFT_WHEEL_PIN1, POWER_MID);
+        analogWrite(RIGHT_WHEEL_PIN1, POWER_MAX);
         break;
       default:
         Serial.println(ch);
@@ -77,5 +82,6 @@ void loop() {
   }
 }
 
-// @TODO. 양쪽 바퀴에 걸리는 힘 차이를 보정하기 위해 양쪽 바퀴에 대한 analogWrite 값을 다르게 설정
+// @TODO. 양쪽 바퀴에 걸리는 힘 차이를 보정하기 위해 양쪽 바퀴에 대한 analogWrite 값을 다르게 설정. POWER_MID를 0으로 변경하면 확인이 용이
 // @TODO. 후진 기능 추가
+// @TODO. Bluetooth STAT 핀을 이용해 연결 여부 표시
